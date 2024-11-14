691a77d69fdd39b5a5d060109cdec5e859f93773
pradanaadn <pradanaadn21@gmail.com>
2024-11-14 20:28:02 +0800

    feat show list Post

--- blogapp/views.py
+++ blogapp/views.py
@@ -3,13 +3,14 @@ from .models import Post
 
 
 def post_list(request):
-    post = Post.published.all()
-    return render(request, "blog/post/detail.html", {"post": post})
+    posts = Post.objects.all()
+    
+    return render(request, "blogapp/post/list.html", {"posts": posts})
 
 
 def post_detail(request, id: int):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, "blog/post/detail.html", {"post": post})
+    return render(request, "blogapp/post/detail.html", {"post": post})
 
 
 # Create your views here.
