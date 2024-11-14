5960865fcb977112f132978cf0447a905ec9b4b1
pradanaadn <pradanaadn21@gmail.com>
2024-11-14 18:27:07 +0800

    feat: create view for show Post list and detail

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+
+def post_list(request):
+    post = Post.published.all()
+    return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_detail(request, id: int):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, "blog/post/detail.html", {"post": post})
+
 
 # Create your views here.
